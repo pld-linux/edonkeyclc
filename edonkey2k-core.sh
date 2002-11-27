80204b4497b6364479abf38050098377b46ea776
djrzulf <djrzulf@pld-linux.org>
2002-11-27 11:43:55 +0000

    0be0edbd97befa34ef2914a753dc9ee  donkey_s_59-4-gaps.tar.gz - edonkey2k-core.sh - a script helping with configure ed2k,
    Changed files:
        edonkey2k-core.sh -> 1.1

---
+++ edonkey2k-core.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+mkdir $HOME/ed2k
+cp /usr/bin/edonkey $HOME/ed2k/donkey
+echo ""
+echo "Done... It's now installed at $HOME/ed2k/ :)"
+echo ""
+echo "Please set \"path to local core\" to $HOME/ed2k/donkey"
+echo ""
+
